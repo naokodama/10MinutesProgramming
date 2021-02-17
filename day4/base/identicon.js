628f75c2d24beb65eeb305ea3994c946bcf8e749
naokodama <loaf.m.wind5420@gmail.com>
2021-02-17 23:35:30 +0900

    generate the hash code from value.

--- day4/base/identicon.js
+++ day4/base/identicon.js
@@ -1,3 +1,8 @@
 $(function () {
     /* Implement here */
+    $("#generate-button").on("click", function(event) {
+        const value = $("#user").val();
+        const hash = md5(value);
+        alert(hash);
+    })
 });
