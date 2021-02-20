9ba8f8261338dabab3ad8183bbbec19544d9641e
naokodama <loaf.m.wind5420@gmail.com>
2021-02-21 00:48:05 +0900

    complete to create the method to generate the Identicon.

--- day4/base/identicon.js
+++ day4/base/identicon.js
@@ -19,9 +19,12 @@ $(function () {
 
         const cvs = $("#identicon").get(0);
         const ctx = cvs.getContext("2d");
+        ctx.clearRect(0, 0, cvs.width, cvs.height);
         const rectSize = 100;
 
-        ctx.fillStyle = "orange";
+        let lastSevenChar = hash.substr(-7);
+
+        ctx.fillStyle = decideColorFromHash(lastSevenChar);
 
         for (i = 0; i < widthSize; i++) {
             for (j = 0; j < centerLine; j++) {
@@ -37,8 +40,27 @@ $(function () {
             };
         };
 
-
-        let lastSevenChar = hash.substr(-7);
-        console.log(lastSevenChar);
     })
+
+    function convertToNewRangeValue(oldVal, oldMin, oldMax, newMin, newMax) {
+        let newVal;
+        const oldRange = oldMax - oldMin;
+        const newRange = newMax - newMin;
+
+        newVal = Math.round(((oldVal - oldMin) / oldRange * newRange) + newMin);
+
+        return newVal;
+    }
+
+    function decideColorFromHash(lastSevenChar) {
+        const orgHue = parseInt(lastSevenChar.substr(0, 3), 16);
+        const orgSatulation = parseInt(lastSevenChar.substr(3, 2), 16);
+        const orgLightness = parseInt(lastSevenChar.substr(5, 2), 16);
+
+        const hue = convertToNewRangeValue(orgHue, 0, parseInt("FFF", 16), 0, 360);
+        const satulation = 65 - convertToNewRangeValue(orgSatulation, 0, parseInt("FF", 16), 0, 20);
+        const lightness = 75- convertToNewRangeValue(orgLightness, 0, parseInt("FF", 16), 0, 20);
+
+        return "hsl(" + hue.toString() + "," + satulation.toString() + "%," + lightness.toString() + "%)";
+    }
 });
